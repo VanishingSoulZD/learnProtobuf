ab5788dd3fa627d59ea57a290089071a5fd042f7
jing.zhou <jing.zhou@shopee.com>
2021-09-10 15:05:38 +0800

    test

--- github.com/VanishingSoulZD/learnProtobufMain/myprotobuf/tmp.pb.go
+++ github.com/VanishingSoulZD/learnProtobufMain/myprotobuf/tmp.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // 	protoc-gen-go v1.26.0
 // 	protoc        v3.12.4
-// source: tmp.proto
+// source: myprotobuf/tmp.proto
 
 package myprotobuf
 
@@ -32,7 +32,7 @@ type Detail struct {
 func (x *Detail) Reset() {
 	*x = Detail{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_tmp_proto_msgTypes[0]
+		mi := &file_myprotobuf_tmp_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -45,7 +45,7 @@ func (x *Detail) String() string {
 func (*Detail) ProtoMessage() {}
 
 func (x *Detail) ProtoReflect() protoreflect.Message {
-	mi := &file_tmp_proto_msgTypes[0]
+	mi := &file_myprotobuf_tmp_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -58,7 +58,7 @@ func (x *Detail) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use Detail.ProtoReflect.Descriptor instead.
 func (*Detail) Descriptor() ([]byte, []int) {
-	return file_tmp_proto_rawDescGZIP(), []int{0}
+	return file_myprotobuf_tmp_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *Detail) GetAge() int64 {
@@ -75,38 +75,38 @@ func (x *Detail) GetName() string {
 	return ""
 }
 
-var File_tmp_proto protoreflect.FileDescriptor
+var File_myprotobuf_tmp_proto protoreflect.FileDescriptor
 
-var file_tmp_proto_rawDesc = []byte{
-	0x0a, 0x09, 0x74, 0x6d, 0x70, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x0a, 0x6d, 0x79, 0x70,
-	0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x22, 0x2e, 0x0a, 0x06, 0x44, 0x65, 0x74, 0x61, 0x69,
-	0x6c, 0x12, 0x10, 0x0a, 0x03, 0x61, 0x67, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x03, 0x52, 0x03,
-	0x61, 0x67, 0x65, 0x12, 0x12, 0x0a, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28,
-	0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x42, 0x41, 0x5a, 0x3f, 0x68, 0x74, 0x74, 0x70, 0x73,
-	0x3a, 0x2f, 0x2f, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x56, 0x61,
-	0x6e, 0x69, 0x73, 0x68, 0x69, 0x6e, 0x67, 0x53, 0x6f, 0x75, 0x6c, 0x5a, 0x44, 0x2f, 0x6c, 0x65,
-	0x61, 0x72, 0x6e, 0x50, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x4d, 0x61, 0x69, 0x6e, 0x2f,
-	0x6d, 0x79, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74,
-	0x6f, 0x33,
+var file_myprotobuf_tmp_proto_rawDesc = []byte{
+	0x0a, 0x14, 0x6d, 0x79, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2f, 0x74, 0x6d, 0x70,
+	0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x0a, 0x6d, 0x79, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62,
+	0x75, 0x66, 0x22, 0x2e, 0x0a, 0x06, 0x44, 0x65, 0x74, 0x61, 0x69, 0x6c, 0x12, 0x10, 0x0a, 0x03,
+	0x61, 0x67, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x03, 0x52, 0x03, 0x61, 0x67, 0x65, 0x12, 0x12,
+	0x0a, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e, 0x61,
+	0x6d, 0x65, 0x42, 0x39, 0x5a, 0x37, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d,
+	0x2f, 0x56, 0x61, 0x6e, 0x69, 0x73, 0x68, 0x69, 0x6e, 0x67, 0x53, 0x6f, 0x75, 0x6c, 0x5a, 0x44,
+	0x2f, 0x6c, 0x65, 0x61, 0x72, 0x6e, 0x50, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x4d, 0x61,
+	0x69, 0x6e, 0x2f, 0x6d, 0x79, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x62, 0x06, 0x70,
+	0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
-	file_tmp_proto_rawDescOnce sync.Once
-	file_tmp_proto_rawDescData = file_tmp_proto_rawDesc
+	file_myprotobuf_tmp_proto_rawDescOnce sync.Once
+	file_myprotobuf_tmp_proto_rawDescData = file_myprotobuf_tmp_proto_rawDesc
 )
 
-func file_tmp_proto_rawDescGZIP() []byte {
-	file_tmp_proto_rawDescOnce.Do(func() {
-		file_tmp_proto_rawDescData = protoimpl.X.CompressGZIP(file_tmp_proto_rawDescData)
+func file_myprotobuf_tmp_proto_rawDescGZIP() []byte {
+	file_myprotobuf_tmp_proto_rawDescOnce.Do(func() {
+		file_myprotobuf_tmp_proto_rawDescData = protoimpl.X.CompressGZIP(file_myprotobuf_tmp_proto_rawDescData)
 	})
-	return file_tmp_proto_rawDescData
+	return file_myprotobuf_tmp_proto_rawDescData
 }
 
-var file_tmp_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
-var file_tmp_proto_goTypes = []interface{}{
+var file_myprotobuf_tmp_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_myprotobuf_tmp_proto_goTypes = []interface{}{
 	(*Detail)(nil), // 0: myprotobuf.Detail
 }
-var file_tmp_proto_depIdxs = []int32{
+var file_myprotobuf_tmp_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
 	0, // [0:0] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -114,13 +114,13 @@ var file_tmp_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_tmp_proto_init() }
-func file_tmp_proto_init() {
-	if File_tmp_proto != nil {
+func init() { file_myprotobuf_tmp_proto_init() }
+func file_myprotobuf_tmp_proto_init() {
+	if File_myprotobuf_tmp_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_tmp_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_myprotobuf_tmp_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*Detail); i {
 			case 0:
 				return &v.state
@@ -137,18 +137,18 @@ func file_tmp_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_tmp_proto_rawDesc,
+			RawDescriptor: file_myprotobuf_tmp_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   1,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
-		GoTypes:           file_tmp_proto_goTypes,
-		DependencyIndexes: file_tmp_proto_depIdxs,
-		MessageInfos:      file_tmp_proto_msgTypes,
+		GoTypes:           file_myprotobuf_tmp_proto_goTypes,
+		DependencyIndexes: file_myprotobuf_tmp_proto_depIdxs,
+		MessageInfos:      file_myprotobuf_tmp_proto_msgTypes,
 	}.Build()
-	File_tmp_proto = out.File
-	file_tmp_proto_rawDesc = nil
-	file_tmp_proto_goTypes = nil
-	file_tmp_proto_depIdxs = nil
+	File_myprotobuf_tmp_proto = out.File
+	file_myprotobuf_tmp_proto_rawDesc = nil
+	file_myprotobuf_tmp_proto_goTypes = nil
+	file_myprotobuf_tmp_proto_depIdxs = nil
 }
